7f3e24d6afd8a7f8456dc10ca52b26c615847423
Andrew <andrewbui101112@gmail.com>
2019-10-02 20:40:48 +1000

    Initial of apply_filters code

--- src/yolo_feeders/scripts/imageloader.py
+++ src/yolo_feeders/scripts/imageloader.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
-# Simple image listener that looks at ../../../input_images
-# Loads images found, and publishes them to /camera/rgb/image_raw 
+# Simple image listener that looks at ../../../input_images 
+# Loads images found, and publishes them to /camera/rgb/image_raw
 # for YOLO_v3 to process
+# Can supply 'filter' argument to apply filters before YOLO processing
+# Options: filter (optional),
 
 # Created: 22/09/19 - Dustin Haines
-# Last Modified: 22/09/19 - Dustin Haines
+# Last Modified: 02/10/19 - Andrew Bui
 
+import sys
 import os
 import cv2
 import rospkg
@@ -23,62 +26,71 @@ PROCESSED_DIR = fp + '/../../processed_images/'
 # Configuration
 VAL_EXTENSIONS = ['png', 'jpg']
 OUTPUT_TOPIC = 'camera/rgb/image_raw'
+OUTPUT_FILTER_TOPIC = 'apply_filters'
 NODE_NAME = 'image_loader'
 QUERY_RATE = 5.0
 
 def findImage():
-    # Create a publisher
-    pub = rospy.Publisher(OUTPUT_TOPIC, Image, queue_size=1)
-    # Create an anonymous node to avoid any potential conflict
-    rospy.init_node(NODE_NAME, anonymous=True)
-    # Set the rate at which the query is run (in Hz)
-    # 1 query a second currently, YOLO should process quicker than this
-    # but varies based on hardware
-    rate = rospy.Rate(1)
-    # Create a bridge instance which will convert out image into the right format
-    bridge = CvBridge()
+	# Create publishers
+	filterPub = rospy.Publisher(OUTPUT_FILTER_TOPIC, Image, queue_size=1)
+	pub = rospy.Publisher(OUTPUT_TOPIC, Image, queue_size=1)
+	# Create an anonymous node to avoid any potential conflict
+	rospy.init_node(NODE_NAME, anonymous=True)
+	# Set the rate at which the query is run (in Hz)
+	# 1 query a second currently, YOLO should process quicker than this
+	# but varies based on hardware
+	rate = rospy.Rate(1)
+	# Create a bridge instance which will convert out image into the right format
+	bridge = CvBridge()
 
-    # Launch the listener loop
-    while not rospy.is_shutdown():
-        print '\nQuerying for images'
-        # Query the directory
-        dir_contents = os.listdir(IMAGE_DIR)
-        # Only do processing if there is an image to process
-        for item in dir_contents:
-            dir_contents.remove(item) if item.split('.')[-1] not in VAL_EXTENSIONS else None
-        if len(dir_contents) > 0:
-            # Open the first file
-            first_file_name = dir_contents[0]
-            first_file = cv2.imread((IMAGE_DIR + first_file_name), -1)
-            print 'Image found (' + first_file_name + '), processing'
-            try:
-                # Transform into the message
-                yolo_image = bridge.cv2_to_imgmsg(first_file)
-                yolo_image.encoding = 'bgr8'
-                # Publish the message
-                pub.publish(yolo_image)
-                print('Image posted to topic /' + OUTPUT_TOPIC)
-            except CvBridgeError as err:
-                print(err)
-            # Move the processed image
-            os.rename((IMAGE_DIR + first_file_name), (PROCESSED_DIR + first_file_name))
-            print(first_file_name + ' moved to ' + PROCESSED_DIR)
-        else:
-            print 'No images found, sleeping...'
-        # Sleep for next iteration
-        rospy.sleep(QUERY_RATE)
+	# Launch the listener loop
+	while not rospy.is_shutdown():
+		print '\nQuerying for images'
+		# Query the directory
+		dir_contents = os.listdir(IMAGE_DIR)
+		# Only do processing if there is an image to process
+		for item in dir_contents:
+			dir_contents.remove(item) if item.split('.')[-1] not in VAL_EXTENSIONS else None
+		if len(dir_contents) > 0:
+			# Open the first file
+			first_file_name = dir_contents[0]
+			first_file = cv2.imread((IMAGE_DIR + first_file_name), -1)
+			print 'Image found (' + first_file_name + '), processing'
+			try:
+				# Transform into the message
+				yolo_image = bridge.cv2_to_imgmsg(first_file)
+				yolo_image.encoding = 'bgr8'
+				# Checks if arguments are specified
+				if len(sys.argv) == 2:
+					# Check if filter argument is specified
+					if sys.argv[1] == 'filter':
+						# Publish to apply_filters node
+						filterPub.publish(yolo_image); 
+				else:             
+					# Publish the message
+					pub.publish(yolo_image)
+					#print('Image posted to topic /' + OUTPUT_TOPIC)
+			except CvBridgeError as err:
+				print(err)
+			# Move the processed image
+			os.rename((IMAGE_DIR + first_file_name), (PROCESSED_DIR + first_file_name))
+			print(first_file_name + ' moved to ' + PROCESSED_DIR)
+		else:
+			print 'No images found, sleeping...'
+		# Sleep for next iteration
+		rospy.sleep(QUERY_RATE)
 
 if __name__ == '__main__':
-    print 'Image Loader launching with following values'
-    print 'IMAGE_DIR: ' + IMAGE_DIR
-    print 'PROCESSED_DIR: ' + PROCESSED_DIR
-    print 'VAL_EXTENSIONS: ' + str(VAL_EXTENSIONS)
-    print 'OUTPUT_TOPIC: ' + OUTPUT_TOPIC
-    print 'NODE_NAME: ' + NODE_NAME
-    print 'QUERY_RATE: ' + str(QUERY_RATE)
-    print '\n'
+	print 'Image Loader launching with following values'
+	print 'IMAGE_DIR: ' + IMAGE_DIR
+	print 'PROCESSED_DIR: ' + PROCESSED_DIR
+	print 'VAL_EXTENSIONS: ' + str(VAL_EXTENSIONS)
+	print 'OUTPUT_TOPIC: ' + OUTPUT_TOPIC
+	print 'NODE_NAME: ' + NODE_NAME
+	print 'QUERY_RATE: ' + str(QUERY_RATE)
+	print '\n'
     
-    try:
-        findImage()
-    except rospy.ROSInterruptException:
-        print '\nShutting down Image Loader'
+	try:
+		findImage()
+	except rospy.ROSInterruptException:
+		print '\nShutting down Image Loader'
